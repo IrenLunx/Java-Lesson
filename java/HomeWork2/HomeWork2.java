343648833cebd0683f3e066932a389c66583fee2
Irina <ideryusheva@inbox.ru>
2022-09-07 22:14:40 +0700

    HomeWork2

--- java/HomeWork2/HomeWork2.java
+++ java/HomeWork2/HomeWork2.java
@@ -5,8 +5,19 @@ import java.util.Scanner;
 
 public class HomeWork2 {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in)
-        System.out.println("Введите число ");
-    }
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите число n: ");
+        int num = in.nextInt();
+        in.close();
 
+        if (num < 1) System.out.println("Решения нет!");
+        else {
+            int res = triangleVal(num);
+            System.out.println(res);
+        }
+    }
+    static int triangleVal(int num) {
+        if (num == 0) return 0;
+        return triangleVal(num - 1) + num;
+    }
 }
